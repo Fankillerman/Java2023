92285b197194f7ef7e5e94fc0b939c7c8f106b0a
Fankillerman <nice.sasha.03@gmail.com>
2024-01-17 15:31:05 +0100

    Test02

---
+++ src/main/java/org/ticketsystem/BasicTicket.java
@@ -0,0 +1,19 @@
+package org.ticketsystem;
+
+public class BasicTicket implements Ticket {
+    private final double price;
+
+    public BasicTicket(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Basic Ticket";
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+}
